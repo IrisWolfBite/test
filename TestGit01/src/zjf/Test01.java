daba88a2e49704c27a78e3dd6ab54c04070bedb7
zhoujunfeng <junefngZhou@163.com>
2019-12-08 22:56:45 +0800

    Eclipse上Git的使用

---
+++ TestGit01/src/zjf/Test01.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package zjf;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author junefngZhou@163.com
+ * <p>Description: 在Eclipse上Git的使用
+ * @date 2019年12月8日 
+ */
+public class Test01 {
+	public static void main(String[] args) {
+		System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "-----hello word!");
+	}
+}
