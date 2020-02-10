c13ba5a496fc636f62c582eeed3d8ea483de3b36
zhoujunfeng <junefngZhou@163.com>
2020-02-10 11:32:49 +0800

    add test02/test03/testMain04/tt

--- TestGit01/src/zjf/Test01.java
+++ TestGit01/src/zjf/Test01.java
@@ -14,5 +14,7 @@ import java.util.Date;
 public class Test01 {
 	public static void main(String[] args) {
 		System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "-----hello word!");
+		String string = "abcd";
+		System.out.print("ABC" + string.substring(0,0) + "DEF");
 	}
 }
